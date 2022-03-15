045edff72cce9a1b3eba92d50d5bbc275c008439
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-15 10:20:51 -0300

    DepartmentDaoJDBC and Program2

---
+++ src/application/Program2.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
