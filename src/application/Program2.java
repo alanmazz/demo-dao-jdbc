726d0bd730ff7134940e59cd061a9d5b3a353a1e
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-15 10:56:20 -0300

    Insert implementation at Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,14 +2,21 @@ package application;
 
 import java.util.Scanner;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		
-		
-		
+		System.out.println("\n=== TEST 1: department insert=====");
+		Department newDep = new Department(8, "Music");
+		depDao.insert(newDep);
+		System.out.println("Inserted! New id = " + newDep.getId());
 		
 		
 		sc.close();
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,13 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -16,8 +21,27 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO department "
+					+ "(Id, Name) "
+					+ "VALUES "
+					+ "(?, ?)");
+			
+			st.setInt(1, obj.getId());
+			st.setString(2, obj.getName());
+			
+			st.executeUpdate();
+			
+			
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
